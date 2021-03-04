aa094f36db1faf64759c9d9d999689e35397517f
Nikhil Bhatia <nikhilbhatia002@gmail.com>
2021-03-04 20:06:22 +0530

    update(sidebar): Integrated array based sidebar as in dc

---
+++ config/sidebar-menu.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    [
+        'name'      => 'MANAGE',
+        'isHeader'  => true,
+        'route'     => null,
+        'children'  => [],
+    ],
+//    [
+//        'name'      => '<i class="nav-icon fa fa-gear"></i><p>Test</p>',
+//        'isHeader'  => false,
+//        'route'     => 'admin.tests.index',
+//        'children'  => [],
+//    ],
+];
