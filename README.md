211aa1603247ee48c157ee4c9655f4c90bb3d61b
Nikhil Bhatia <nikhilbhatia002@gmail.com>
2021-03-05 13:30:57 +0530

    chores(installation): Command implemented.

--- README.md
+++ README.md
@@ -10,14 +10,10 @@
     3. DB credentials
     4. Email Credentials
     5. Update `MY_SQL_VERSION`, if it is below `8`. **this is mandatory, otherwise, it would result in error while migrating.**
-5. Run `npm install` & `npm dev`, if wanted to use scaffolded Tailwind CSS. (Make sure you have Node v12 at least installed)
-6. Run `php artisan migrate`.
-7. Run `php artisan codiksh:deploy`.
-8. Run `php artisan ide-helper:meta`.
-9. Run `php artisan ide-helper:generate`.
-10. Run `php artisan db:seed --class=RolesSeeder`
-11. Run `php artisan db:seed --class=AdminSeeder`
-12. Configure `medialibrary` config for `LocalStore`.
+5. Make sure, that at least `v12` of `Node.JS` is installed on your system.
+6. Copy `version.yml` file from `\resources\` directory to `\config\` directory.
+6. Run `php artisan codiksh:install-template`.
+7. Configure `medialibrary` config for `LocalStore`.
 
 
 ##Breaking changes as compare to L7
---
+++ app/Console/Commands/InstallationCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallationCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'codiksh:install-template';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Installs template.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        system('npm install');
+        $this->call('migrate');
+        $this->call('codiksh:deploy');
+        $this->call('ide-helper:meta');
+        $this->call('ide-helper:generate');
+        $this->call('db:seed --class=RolesSeeder');
+        $this->call('db:seed --class=AdminSeeder');
+        return 1;
+    }
+}
