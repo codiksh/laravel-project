d7bac3a6f7288649569fe231b3ad2046568a1609
Nikhil Bhatia <nikhilbhatia002@gmail.com>
2021-03-12 15:24:09 +0530

    update(requests): Added left over BaseRequest class file

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
+
+class BaseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Overridden
+     * To add, additional Error Hooks
+     *
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function (Validator $validator) {
+            $this->after($validator);
+        });
+    }
+
+    /**
+     * function to manage after-request-has-been-validated.
+     * @param $validator
+     */
+    protected function after($validator)
+    {
+        //
+    }
+}
