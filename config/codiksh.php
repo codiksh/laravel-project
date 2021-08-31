f3043afe9a84b2ec514198d0b25acf60b7cd10e5
Hemaxi Goyani <hemaxi@codiksh.in>
2021-08-31 09:21:21 +0530

    Update(Command) : Update config file for deploying command

--- config/codiksh.php
+++ config/codiksh.php
@@ -3,4 +3,6 @@
 return [
     'mysql-version' => env('MY_SQL_VERSION', 8),
     'short_name' => env('APP_SHORT_NAME', 'CDK'),
+    'descriptive_name' => env('APP_NAME_DESCRIBE', 'Codiksh - High Performant Laravel Application Development Services'),
+    'repo_path' => env('REPO_PATH'),
 ];
