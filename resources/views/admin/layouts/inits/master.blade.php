fd1eda5e7e856df876ee12d77a67404937d42765
Hemaxi Goyani <hemaxi@codiksh.in>
2022-02-02 18:00:57 +0530

    feat(file upload dropzone) : Required files added

--- resources/views/admin/layouts/inits/master.blade.php
+++ resources/views/admin/layouts/inits/master.blade.php
@@ -12,3 +12,5 @@
 @include('admin.layouts.inits.datatables_action')
 @include('admin.layouts.inits.dropzoneSImage')
 @include('admin.layouts.inits.dropzoneMImage')
+@include('admin.layouts.inits.dropzoneFileUpload')
+@include('admin.layouts.inits.existing_attachments')
