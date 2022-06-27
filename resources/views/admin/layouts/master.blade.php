0162c5bb0dc70f2a71367ad3b9ce1c372c473f5c
Nikhil Bhatia <nikhil@codiksh.in>
2022-06-27 20:10:46 +0530

    chores: Corrected Design Layout

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -2,76 +2,12 @@
 <html lang="en">
 <head>
     @include('admin.layouts.head')
-    <meta charset="UTF-8">
-    <title>@yield('title',config('app.name'))</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <meta name="_token" content="{!! csrf_token() !!}"/>
-
-    @if(isset($si))
-    <link rel="shortcut icon" href="{{ $si['favicon']->favicon }}" type="image/x-icon">
-    @endif
-
-    <!-- Bootstrap 4.0.0 -->
-{{--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">--}}
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/32/bootstrap-toggle.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css">
-
-    <!-- iCheck -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/all.css">
-
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/31/select2.min.css">
-
-    <!-- Ionicons -->
-{{--    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">--}}
-
-    <!-- Animate CSS -->
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/7/animate.min.css">
-
-    <!-- Toastr CSS -->
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/8/toastr.min.css">
-
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/30/waitMe.min.css">
-
-    <!-- Sweet Alert -->
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/9/sweetalert2.min.css">
-    <!-- Jquery Bonsai -->
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/10/jquery.bonsai.css">
-    <!-- Jquery Time picker -->
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/11/jquery.timepicker.css">
-    <!-- Date Range Picker -->
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/12/daterangepicker.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="https://assets.codiksh.in/storage/media/assets/39/adminlte.min.css">
-    <!-- DROPZONE CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
-    <!-- NTB CSS -->
-    <link rel="stylesheet" href="{{ asset('css/ntb.css') }}">
-
-
-    @yield('css')
-    @stack('stackedCss')
-    <style>
-        .card-head-row .float-right .datatables_action .action-button .fas {
-            color: white;
-        }
-
-        .card-head-row .float-right .datatables_action .action-button {
-            padding: 10%;
-            margin-left: 100%;
-        }
-
-        .dropleft .dropdown-menu {
-            padding: 0;
-            z-index: 1040;
-        }
-    </style>
 </head>
 <body class="sidebar-mini hold-transition accent-primary sidebar-collapse fixed layout-fixed" >
 <div class="wrapper">
-    {{--    <div class="preloader flex-column justify-content-center align-items-center">--}}
-    {{--        <img class="animation__shake" src="{{ asset('logo.png') }}" alt="{{ config('app.name') }}'s Logo" height="60" width="60">--}}
-    {{--    </div>--}}
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="{{ asset('icon.jpg') }}" alt="{{ config('app.name') }}'s Logo" height="60" width="60">
+    </div>
     @include('admin.layouts.header')
     @include('admin.layouts.sidebar')
 
@@ -104,37 +40,32 @@
 </div>
 @yield('modal')
 
-<!-- jQuery 3.5.1 -->
-<script src="https://assets.codiksh.in/storage/media/assets/16/jquery.min.js"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/17/moment.min.js"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/18/popper.min.js"></script>
-<!-- Bootstrap 4.0.0 -->
-<script src="https://assets.codiksh.in/storage/media/assets/15/bootstrap.min.js"></script>
-<!-- Date Range Picker -->
-<script src="https://assets.codiksh.in/storage/media/assets/14/daterangepicker.js"></script>
-
-<script src="https://assets.codiksh.in/storage/media/assets/19/jquery.slimscroll.min.js"></script>
-
-<script src="https://assets.codiksh.in/storage/media/assets/20/icheck.min.js"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/21/select2.min.js"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/22/toastr.min.js"></script>
-<script src="{{ asset('/assets/bower_components/tinymce/tinymce.min.js') }}"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/24/waitMe.min.js"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/25/sweetalert2.min.js"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/26/jquery.qubit.js"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/27/jquery.bonsai.js"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/28/jquery.timepicker.js"></script>
-<script src="https://assets.codiksh.in/storage/media/assets/29/url_slug.js"></script>
+<script src="{{ asset('assets/jquery_3.5.1/jquery.min.js') }}"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="{{ asset('assets/moment_2.15.1/moment.min.js') }}"></script>
+<script src="{{ asset('assets/popper/popper.min.js') }}"></script>
+<script src="{{ asset('assets/bootstrap_4.0.0/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/daterangepicker_3.0.5/js/daterangepicker.js') }}"></script>
+{{--<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>--}}
+<script src="{{ asset('assets/slimscroll_1.3.8/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ asset('assets/icheck_1.0.2/icheck.min.js') }}"></script>
+<script src="{{ asset('assets/select2_4.0.13/js/select2.min.js') }}"></script>
+<script src="{{ asset('assets/toastr/js/toastr.min.js') }}"></script>
+<script src="{{ asset('assets/bower_components/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('assets/waitMe_1.15/js/waitMe.min.js') }}"></script>
+<script src="{{ asset('assets/sweetalert2/js/sweetalert2.min.js') }}"></script>
+<script src="{{ asset('assets/qubit/jquery.qubit.js') }}"></script>
+<script src="{{ asset('assets/jquery_bonsai/js/jquery.bonsai.js') }}"></script>
+<script src="{{ asset('assets/timepicker/js/jquery.timepicker.js') }}"></script>
+<script src="{{ asset('assets/url_slug/url_slug.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/overlayscrollbars/1.13.0/js/jquery.overlayScrollbars.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+<script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap-switch-button@1.1.0/dist/bootstrap-switch-button.min.js"></script>
 
 <!-- AdminLTE App -->
-<script src="https://assets.codiksh.in/storage/media/assets/38/adminlte.min.js"></script>
-
-
-<script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
-
-=======
-<script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 
 <script defer>
     $.ajaxSetup({headers: { 'X-CSRF-Token' : $('meta[name=_token]').attr('content') }});
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,26 +1,22 @@
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar elevation-4 sidebar-dark-primary">
-    <!-- Brand Logo -->
-    <a href="{{ route('dashboard') }}" class="brand-link navbar-primary text-center">
-        <span class="logo-mini"><strong>{{ config('codiksh.short_name') }}</strong></span>
-        <span class="brand-text font-weight-bold">{{ config('app.name') }}</span>
+<aside class="main-sidebar elevation-4 sidebar-dark-primary ">
+    <a href="{{ route('dashboard') }}" class="brand-link">
+        <img src="{{ asset('icon.jpg') }}" alt="{{ config('app.name') }}'s logo" class="brand-image img-circle elevation-3" style="">
+        <h4 class="brand-text">{{ config('app.name') }}</h4>
     </a>
-    <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar Menu -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image mt-2">
-                <img style="object-fit: cover;" src="{{ \Illuminate\Support\Facades\Auth::user()->avatar_url['100'] }}" class="img-circle elevation-2" alt="{{ \Illuminate\Support\Facades\Auth::user()->name }}'s Avatar">
+            <div class="image" style="display: flex; align-items: center">
+                <img src="{{ \Illuminate\Support\Facades\Auth::user()->avatar_url['100'] }}" class="img-circle elevation-2" alt="{{ \Illuminate\Support\Facades\Auth::user()->name }}'s Avatar">
             </div>
             <div class="info">
-                <a href="javascript:void(0);" class="d-block">
-                    {{ \Illuminate\Support\Facades\Auth::user()->name }}
-                    <br/><small style="white-space: initial">{{ \Illuminate\Support\Facades\Auth::user()->getRoleNames()->join(', ') }}</small>
+                <a href="#" class="text-light">
+                    <strong>{{ \Illuminate\Support\Facades\Auth::user()->name }}</strong>
+                    <br/><small class="user-role" style="white-space: initial">{{ \Illuminate\Support\Facades\Auth::user()->getRoleNames()->join(', ') }}</small>
                 </a>
             </div>
         </div>
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column nav-flat nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
                 @include('admin.layouts.menu')
             </ul>
         </nav>
