4a0508b64f7b113cb6a7df1580c0e8c8a3512e31
Nikhil Bhatia <nikhil@codiksh.in>
2022-07-02 18:06:31 +0530

    fix(dropzone): A minor correction relating to previous commit

--- resources/views/admin/layouts/scripts/dzSingleImageField.blade.php
+++ resources/views/admin/layouts/scripts/dzSingleImageField.blade.php
@@ -1,12 +1,12 @@
 <div class="{{ !empty($className) ? $className : 'col-sm-3' }} dz-sImageOuterContainer">
     <div class="dz-sImageContainer mayContainError fastAnimation dropzone dz-clickable avatar-upload" id="{{$elementId}}" tabindex="1">
-        <div class="single-image-upload dz-clickable {{ !empty($hasMedia) ? 'd-none' : '' }}">
+        <div class="single-image-upload dz-clickable {{ !empty($hasMedia) && $hasMedia ? 'd-none' : '' }}">
             <div class='dz-sImagePlaceholderText text-center'>
                 {!! empty($placeHolderText) ? 'Drop/Select Avatar here <br/>(Max: 1 MB)' : $placeHolderText !!}
             </div>
         </div>
         <div class="preview-container"></div>
-        @if(!empty($hasMedia))
+        @if(!empty($hasMedia) && $hasMedia)
             <div class='dz-sImagePreview'>
                 <img class='dz-sImagePreviewImg' alt='Avatar'
                      src='{{ $previewUrl }}'
