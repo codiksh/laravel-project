bb1a77365f2bdd40f3756504aad4c8adfd3c64ca
Nikhil Bhatia <nikhil@codiksh.in>
2022-07-02 18:38:56 +0530

    update(design-upgrades): Various left out changes regarding new design upgrades

--- config/sidebar-menu.php
+++ config/sidebar-menu.php
@@ -2,7 +2,8 @@
 
 return [
     [
-        'name'      => '<i class="nav-icon fa-duotone fa-users"></i><p>Users</p>',
+        'name'      => 'Users',
+        'icon'      => '<i class="nav-icon fa-duotone fa-users"></i>',
         'isHeader'  => false,
         'route'     => 'admin.users.index',
         'children'  => [],
